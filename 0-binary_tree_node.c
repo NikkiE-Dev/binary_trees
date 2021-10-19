cf8c29aa9db97e4c0a33f06b60e34a5cdc8d3217
NikkiE-Holberton <78047506+NikkiE-Holberton@users.noreply.github.com>
2021-10-18 22:07:38 -0500

    Making file betty compliant

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,20 +8,20 @@
  * Return: new node or NULL
  */
 
- binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
- {
-    binary_tree_t *newNode = NULL;
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode = NULL;
 
-    newNode = malloc(sizeof(binary_tree_t));
+	newNode = malloc(sizeof(binary_tree_t));
 
-    if (newNode == NULL)
-    {
-        return (NULL);
-    }
-    newNode->n = value;
-    newNode->parent = parent;
-    newNode->left  = NULL;
-    newNode->right = NULL;
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left  = NULL;
+	newNode->right = NULL;
 
-    return (newNode);
- }
+	return (newNode);
+}
