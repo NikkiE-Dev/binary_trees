5fa0fa94ceacd780f6776e7444788f3ed65530e3
NikkiE-Holberton <78047506+NikkiE-Holberton@users.noreply.github.com>
2021-10-19 01:50:14 -0500

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -75,7 +75,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		height_left = binary_tree_height(tree->left);
 		height_right = binary_tree_height(tree->right);
 
-		if (height_left > height_right)
+		if (height_left >= height_right)
 		{
 			return (height_left + 1);
 		}
