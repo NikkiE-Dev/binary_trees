fcafb6ca6dee46d196b8ad154e3dc6025f6801b5
NikkiE-Holberton <78047506+NikkiE-Holberton@users.noreply.github.com>
2021-10-18 23:36:09 -0500

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,11 +15,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		if (tree->left)
 		{
-			height_left - 1 + binary_tree_height(tree->left)
+			height_left = 1 + binary_tree_height(tree->left);
 		}
 		if (tree->right)
 		{
-			height_right - 1 + binary_tree_height(tree->right)
+			height_right = 1 + binary_tree_height(tree->right);
 		}
 		if (height_left >= height_right && height_left != 0)
 		{
