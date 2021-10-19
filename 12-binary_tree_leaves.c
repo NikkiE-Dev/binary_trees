7678f288213583983614b6cb7135981ecaf807e1
NikkiE-Holberton <78047506+NikkiE-Holberton@users.noreply.github.com>
2021-10-18 23:54:08 -0500

    Create 12-binary_tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measures the depth of a node in a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure the size
+ * Return: NULL or 0
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left_size, right_size, measured_size;
+
+	if (tree)
+	{
+		if (tree->left)
+		{
+			left_size = binary_tree_size(tree->left);
+		}
+		if (tree->right)
+		{
+			right_size = binary_tree_size(tree->right);
+		}
+		measured_size = (left_size + right_size) + 1;
+		return (measured_size);
+	}
+	return (0);
+}
