66c47656db7813e46518ed5b48b8584eda816d6e
NikkiE-Holberton <78047506+NikkiE-Holberton@users.noreply.github.com>
2021-10-19 01:16:12 -0500

    Create 16-binary_tree_is_perfect.c

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,90 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_perfect - checks if a binary tree is perfect
+ *
+ * @tree: Pointer to the root node of the tree to check
+ * Return: 1 if tree is perfect or 0 if not
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int height_left, height_right, measured_height, measured_size;
+
+	if (tree)
+	{
+		height_left = binary_tree_height(tree->left);
+		height_right = binary_tree_height(tree->right);
+		measured_height = height_right + height_left;
+		measured_size = binary_tree_size(tree);
+
+		if ((measured_size == (1 << measured_height)) && (height_left == height_right))
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (0);
+}
+/**
+ * binary_tree_height -measures the height of a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure the height.
+ * Return: nothing
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int height_left = 0, height_right = 0;
+
+	if (tree)
+	{
+		if (tree->left)
+		{
+			height_left = 1 + binary_tree_height(tree->left);
+		}
+		if (tree->right)
+		{
+			height_right = 1 + binary_tree_height(tree->right);
+		}
+		if (height_left >= height_right && height_left != 0)
+		{
+			return (height_left);
+		}
+		if (height_right >= height_left && height_right != 0)
+		{
+			return (height_right);
+		}
+	}
+	return (0);
+}
+
+/**
+ * binary_tree_size - measures the depth of a node in a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure the size
+ * Return: NULL or 0
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left_size = 0, right_size = 0, measured_size = 0;
+
+	if (tree)
+	{
+		if (tree->left)
+		{
+			left_size = binary_tree_size(tree->left);
+		}
+		if (tree->right)
+		{
+			right_size = binary_tree_size(tree->right);
+		}
+		measured_size = (left_size + right_size) + 1;
+		return (measured_size);
+	}
+	return (0);
+}
