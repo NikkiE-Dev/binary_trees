fe893262ecd45ae3b1d99b5813fd1a1cb158d578
NikkiE-Holberton <78047506+NikkiE-Holberton@users.noreply.github.com>
2021-10-19 00:38:12 -0500

    Create 14-binary_tree_balance.c

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,58 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - measures the depth of a node in a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure the size
+ * Return: balance of a binary tree or 0
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int height_left, height_right, balanced_size;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+	height_left = binary_tree_height(tree->left);
+	height_right = binary_tree_height(tree->right);
+	balanced_size = height_left - height_right;
+	}
+	return (balanced_size);
+}
+
+/**
+ * binary_tree_height -measures the height of a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure the height.
+ * Return: height of a binary tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int height_left = 0, height_right = 0;
+
+	if (tree)
+	{
+		if (tree->left)
+		{
+			height_left = 1 + binary_tree_height(tree->left);
+		}
+		if (tree->right)
+		{
+			height_right = 1 + binary_tree_height(tree->right);
+		}
+		if (height_left >= height_right && height_left != 0)
+		{
+			return (height_left);
+		}
+		if (height_right >= height_left && height_right != 0)
+		{
+			return (height_right);
+		}
+	}
+	return (0);
+}
